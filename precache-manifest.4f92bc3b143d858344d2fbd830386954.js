4d8bddfeb5a9844de236ec4669ac816203cd5625
Jeremiah Bonney <jjbonney4@gmail.com>
2022-03-26 12:23:21 -0400

    Updates

---
+++ precache-manifest.4f92bc3b143d858344d2fbd830386954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba53a1b718c8b9d6194dba511c0a071e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec5d1e35c609771047b",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "5a6de77cd5a580211828",
+    "url": "/static/js/2.95b22ae9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.95b22ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec5d1e35c609771047b",
+    "url": "/static/js/main.b01394e4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
